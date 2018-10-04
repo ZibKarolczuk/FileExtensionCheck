f3d677248f79738c3888b694e0d4f2565e1ae1df
Zbigniew Karolczuk <karolczuk.z@gmail.com>
2018-10-04 13:04:17 +0200

    Test if can read mime of given file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main Function");
+
+        String filename = "./file1.png"; // Filename in the main project directory:
+
+        try {
+
+            Path source = Paths.get(filename); // An object alllocating the file:
+            System.out.println(Files.probeContentType(source)); // Testing if can get a type of the file form MIME (Magic numbers):
+
+        } catch (IOException e){
+            System.out.println("No such file exists"); // Exception
+        }
+
     }
 }

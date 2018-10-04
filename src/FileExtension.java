7f1ec3a11a6876199b65dbd956490d83674cee61
Zbigniew Karolczuk <karolczuk.z@gmail.com>
2018-10-04 13:13:57 +0200

    Good constructor in service class, commented not necessary stuff in Main class

--- src/FileExtension.java
+++ src/FileExtension.java
@@ -1,8 +1,28 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class FileExtension {
 
     private String filename;
+    private String mimeType;
 
     public FileExtension(String filename) {
         this.filename = filename;
+        Path source = Paths.get(filename); // An object alllocating the file:
+        try {
+            this.mimeType = Files.probeContentType(source); //Getting MIME type of the file
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public String getMimeType() {
+        return mimeType;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,14 @@ public class Main {
 
         String filename = "./file1.png"; // Filename in the main project directory:
 
-        try {
-
-            Path source = Paths.get(filename); // An object alllocating the file:
-            System.out.println(Files.probeContentType(source)); // Testing if can get a type of the file form MIME (Magic numbers):
-
-        } catch (IOException e){
-            System.out.println("No such file exists"); // Exception
-        }
+//        try {
+//
+//            Path source = Paths.get(filename); // An object alllocating the file:
+//            System.out.println(Files.probeContentType(source)); // Testing if can get a type of the file form MIME (Magic numbers):
+//
+//        } catch (IOException e){
+//            System.out.println("No such file exists"); // Exception
+//        }
 
     }
 }
